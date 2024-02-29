4706bc195aa92c354ebc48188d6dd428c234841d
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-02-29 18:57:59 +0530

    Added Button Component in the button folder

---
+++ src/components/buttons/ButtonPrimary.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'; 
+import { Button } from 'primereact/button';
+
+export default function ButtonPrimary() {
+    return (
+        <div className="card flex justify-content-center">
+            <Button label="Submit" />
+        </div>
+    )
+}
+        
