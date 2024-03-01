3079570aec7d7cd7ce0d75bf446181830251fd99
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-03-01 21:18:06 +0530

    Remove unused code and buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import { Button } from "primereact/button";
-// import DropdownMenu from "./components/DropdownMenu";
-import ButtonPrimary from "./components/buttons/ButtonPrimary";
 
 function App() {
   return (
     <>
-      {/* <DropdownMenu/> */}
-      <ButtonPrimary label="Submit" style=" bg-red-700 w-fit text-white " />
-      <ButtonPrimary label="Cancel" style=" bg-sky-700 w-fit text-white " />
+      
     </>
   );
 }
