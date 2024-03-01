2840292914a86205292a890c092eac32b3fa3605
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-03-01 21:35:04 +0530

    Remove unused components and add dependencies of Flowbite UI Library and structured the component folders

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,12 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/lib/esm/**/*.js'
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  // eslint-disable-next-line no-undef
+  plugins: [require('flowbite/plugin')],
 }
 
