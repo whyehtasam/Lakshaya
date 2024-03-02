423829f8a520c6c9abe95a84ee6a832028680c1c
Hanzala <hanzalatafzeel44@gmail.com>
2024-03-02 12:55:23 +0530

    Carousel Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-
+import Button from "./components/buttons/Button";
+import Testimonnial from "./components/carousels/Carousel";
 function App() {
   return (
     <>
-      
+      <Testimonnial />
     </>
   );
 }
