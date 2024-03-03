0f31dbf726a02df66e673d3fd8cd2e54ffa251da
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-03-03 15:37:53 +0530

    Update Hero component text size for smaller screens

--- src/apps/landing/Hero.jsx
+++ src/apps/landing/Hero.jsx
@@ -10,8 +10,8 @@ const Hero = () => {
             <h1 className="text-4xl font-bold text-gray-800 md:text-5xl lg:text-7xl">Guiding Minds,</h1>
             <h1 className="text-4xl font-bold text-gray-800 md:text-5xl lg:text-7xl">Building Generations</h1>
             <div>
-                <p className="mt-4 text-lg text-gray-600 md:text-xl lg:text-2xl">JEE and NEET: A new paradigm in motivation and success direction.</p>
-                <p className="mt-4 text-lg text-gray-600 md:text-xl lg:text-2xl">JEE और NEET: नए प्रेरणा और सफलता की दिशा में एक नया मोड़।</p>
+                <p className="mt-4 sm:text-lg text-gray-600 md:text-xl lg:text-2xl">JEE and NEET: A new paradigm in motivation and success direction.</p>
+                <p className="mt-4 sm:text-lg text-gray-600 md:text-xl lg:text-2xl">JEE और NEET: नए प्रेरणा और सफलता की दिशा में एक नया मोड़।</p>
             </div>
             <div className='flex gap-5 mt-10'>
                 <PrimaryButton className='flex gap-2 px-6 py-3 sm:text-lg font-semibold tracking-wider rounded-md outline outline-2 outline-red-800 outline-offset-2 hover:bg-red-700' label='Enroll Now'/>
