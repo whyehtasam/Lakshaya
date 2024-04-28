3f11377c745834640528eca3783a4bc1e8bb6544
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-04-28 22:44:10 +0530

    Added header component in about section and contact us

--- src/apps/aboutUs/AboutUs.jsx
+++ src/apps/aboutUs/AboutUs.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Header from './Header'
+import Container from '../../components/wrappers/Container'
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <>
+    <Container>
+
+    <Header/>
+    </Container>
+    </>
   )
 }
 
