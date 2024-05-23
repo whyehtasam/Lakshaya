2968f8f7f26cf72bba7832d851be60cb6949d91b
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-05-23 18:45:25 +0530

    Revert "refactor: Update Slider component to remove unnecessary left and right controls"
    This reverts commit b59b106c3c4938bc139be386e1273e61117e6ea7.
    reversed the changes made by shahnawaz to my commit

--- src/components/carousels/Slider.jsx
+++ src/components/carousels/Slider.jsx
@@ -5,7 +5,7 @@ import { Carousel } from "flowbite-react";
 export default function Slider({children,className}) {
   return (
     <div className={`${className}`}>
-      <Carousel pauseOnHover indicators={false} slideInterval={3000}>
+      <Carousel pauseOnHover indicators={false} slideInterval={3000} leftControl=" " rightControl=" ">
         {children}
       </Carousel>
     </div>
