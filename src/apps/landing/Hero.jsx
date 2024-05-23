73b48e9ac2a5f43658f0d5b8c67cb1ef9a5f7375
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-05-23 20:04:39 +0530

    refactor: Replace Card component with ResultCard component in Landing page , Hero component-- fixed code

--- src/apps/landing/Hero.jsx
+++ src/apps/landing/Hero.jsx
@@ -1,27 +1,46 @@
-import React from 'react'
-import PrimaryButton from '../../components/buttons/PrimaryButton'
-import SecondaryButton from '../../components/buttons/SecondaryButton'
+import React from "react";
+import PrimaryButton from "../../components/buttons/PrimaryButton";
+import SecondaryButton from "../../components/buttons/SecondaryButton";
+import teachers from "../../assets/TeachersHero.jpg"
+
 
 const Hero = () => {
   return (
-    <div>
-        <div className="flex flex-col justify-start gap-3 sm:gap-6 my-7">
-            <h1 className="text-4xl font-bold text-yellow-300 md:text-5xl lg:text-7xl">The Lakshaya:</h1>
-            <h1 className="text-4xl font-bold text-gray-800 md:text-5xl lg:text-7xl">Forum for</h1>
-            <h1 className="text-4xl font-bold text-gray-800 md:text-5xl lg:text-7xl">IIT-JEE and Medical</h1>
-            <div>
-                <p className="mt-4 text-gray-600 sm:text-lg md:text-xl lg:text-2xl">JEE and NEET: A new paradigm in motivation and success direction.</p>
-                <p className="mt-4 text-gray-600 sm:text-lg md:text-xl lg:text-2xl">JEE और NEET: नए प्रेरणा और सफलता की दिशा में एक नया मोड़।</p>
-            </div>
-            <div className='flex gap-5 mt-5 sm:mt-10'>
-                <PrimaryButton className='flex gap-2 font-semibold tracking-wider rounded-md sm:px-6 sm:py-3 sm:text-lg outline outline-2 outline-red-800 outline-offset-2 hover:bg-red-700' label='Enroll Now'/>
-                <SecondaryButton className='font-bold tracking-wider text-black bg-white rounded-md sm:px-6 sm:py-3 sm:text-lg outline outline-2 outline-red-800 outline-offset-2 hover:' label='Contact Us'/>
-            </div>
-            
-            
+    <div className="md:flex justify-between items-center">
+      <div className="flex flex-col justify-start gap-3 sm:gap-6 my-7 md:w-[60vw]" >
+        <h1 className="text-4xl font-bold text-red-800 md:text-5xl lg:text-7xl">
+          The Lakshaya:
+        </h1>
+        <h1 className="text-4xl font-bold text-gray-800 md:text-5xl lg:text-7xl">
+          Guiding Minds,
+        </h1>
+        <h1 className="text-4xl font-bold text-gray-800 md:text-5xl lg:text-7xl">
+          Building Generations
+        </h1>
+        <div>
+          <p className="mt-4 text-gray-600 sm:text-lg md:text-xl lg:text-2xl">
+            JEE and NEET: A new paradigm in motivation and success direction.
+          </p>
+          <p className="mt-4 text-gray-600 sm:text-lg md:text-xl lg:text-2xl">
+            JEE और NEET: नए प्रेरणा और सफलता की दिशा में एक नया मोड़।
+          </p>
         </div>
+        <div className="flex gap-5 mt-5 sm:mt-10">
+          <PrimaryButton
+            className="flex gap-2 font-semibold tracking-wider rounded-md sm:px-6 sm:py-3 sm:text-lg outline outline-2 outline-red-800 outline-offset-2 hover:bg-red-700"
+            label="Enroll Now"
+          />
+          <SecondaryButton
+            className="font-bold tracking-wider text-black bg-white rounded-md sm:px-6 sm:py-3 sm:text-lg outline outline-2 outline-red-800 outline-offset-2 hover:"
+            label="Contact Us"
+          />
+        </div>
+      </div>
+      <div>
+        <img src={teachers} alt="" className=" md:rounded-tl-[108px] md:rounded-br-[108px] md:rounded-tr-md md:rounded-bl-md rounded-md "/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/apps/landing/Landing.jsx
+++ src/apps/landing/Landing.jsx
@@ -1,3 +1,6 @@
+import ResultCard from "../../components/cards/ResultCard";
+import Slider from "../../components/carousels/Slider";
+import Header from "../../components/header/Header";
 import BlobCircle from "../../components/wrappers/BlobCircle";
 import Container from "../../components/wrappers/Container";
 import Batch from "./Batch";
@@ -9,8 +12,13 @@ import Results from "./Results";
 // import ScholorshipHero from "./ScholorshipHero";
 
 import Teams from "./Teams";
+import { ResultData } from "./data";
 
 const Landing = () => {
+  const chunks = [];
+  for (let i = 0; i < ResultData.length; i += 3) {
+    chunks.push(ResultData.slice(i, i + 3));
+  }
   return (
     <div className="relative overflow-hidden">
       <Container className=" landing">
@@ -18,6 +26,35 @@ const Landing = () => {
           <BlobCircle />
         </div>
         <Hero />
+      </Container>
+      <Container className="result pt-0 lg-pt-0">
+        <div className="relative grid place-content-center">
+          <Header
+            title="Best Performance"
+            header="Meet Our Top achievers"
+            description="Best performance in the last academic year of the students of the The Lakshya."
+            titleStyle="lg:text-xl"
+            headerStyle="sm:text-5xl text-black"
+            descStyle="sm:text-lg text-gray-700"
+            className="pb-6 pt-4 text-center"
+          />
+        </div>
+
+        <Slider className="hidden md:block h-[30rem] sm:h-96 ">
+          {chunks.map((chunk, index) => (
+            <div key={index} className="grid md:grid-cols-3 gap-5">
+              {chunk.map((student) => (
+                <ResultCard key={student.name} student={student} />
+              ))}
+            </div>
+          ))}
+        </Slider>
+        <Slider className="block md:hidden h-[30rem]  ">
+          {ResultData.map((student) => (
+            <ResultCard key={student.name} student={student} />
+          ))}
+
+        </Slider>
       </Container>
       <div className="bg-red-900">
         <Container>
