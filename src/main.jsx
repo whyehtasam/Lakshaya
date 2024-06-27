fba941e658e170a4ec1017f92d05bdfb23e326de
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-06-28 00:11:54 +0530

    chore: Update npm dependencies and add @vercel/analytics package

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Analytics } from '@vercel/analytics/react';
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+  <Analytics>
     <App />
-  </React.StrictMode>
+  </Analytics>
+</React.StrictMode>
 );
