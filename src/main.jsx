2169b5f8e1ba249d15931b847f3d26cd22c98df6
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-06-28 00:24:23 +0530

    chore: Update npm dependencies and remove @vercel/analytics package

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { Analytics } from '@vercel/analytics/react';
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <Analytics>
     <App />
-  </Analytics>
-</React.StrictMode>
+  </React.StrictMode>
 );
