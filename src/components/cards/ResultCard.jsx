48523f07ed4d9db747dc2b4be47f5c2d264a7958
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2024-08-03 22:35:49 +0530

    refactor: Remove unused React import in ResultCard component

--- src/components/cards/ResultCard.jsx
+++ src/components/cards/ResultCard.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const ResultCard = ({ student: { image_url, exam, name, description, rank } } ) => {
   return (
     <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 h-full ">
